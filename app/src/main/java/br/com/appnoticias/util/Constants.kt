1f6fdee33134fc20d14fe765f49a73ffc9262cb6
DeniseLeandroDeCastro <dmaria.natal@gmail.com>
2022-08-28 09:18:59 -0300

    Configuração dos endpoints

---
+++ app/src/main/java/br/com/appnoticias/util/Constants.kt
@@ -0,0 +1,9 @@
+package br.com.appnoticias.util
+
+class Constants {
+
+    companion object {
+        const val API_KEY = "dbf43001f1424b708b14bd1333c2564e"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
