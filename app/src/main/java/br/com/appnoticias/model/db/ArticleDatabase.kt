080827351bb216d54a97a35a83985337a0ed476a
DeniseLeandroDeCastro <dmaria.natal@gmail.com>
2022-08-30 20:55:48 -0300

    Implementando a classe Converters

--- app/src/main/java/br/com/appnoticias/model/db/ArticleDatabase.kt
+++ app/src/main/java/br/com/appnoticias/model/db/ArticleDatabase.kt
@@ -4,10 +4,11 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import br.com.appnoticias.model.Article
-import java.util.concurrent.locks.Lock
 
 @Database(entities = [Article::class], version = 1, exportSchema = false)
+@TypeConverters(Converters::class)
 abstract class ArticleDatabase : RoomDatabase() {
 
     abstract fun getArticleDao(): ArticleDao
--- app/src/main/java/br/com/appnoticias/model/db/Converters.kt
+++ app/src/main/java/br/com/appnoticias/model/db/Converters.kt
@@ -1,4 +1,17 @@
 package br.com.appnoticias.model.db
 
+import androidx.room.TypeConverter
+import br.com.appnoticias.model.Source
+
 class Converters {
+
+    @TypeConverter
+    fun fromSource(source: Source) : String {
+        return source.name
+    }
+
+    @TypeConverter
+    fun toSource(name: String): Source {
+        return Source(name, name)
+    }
 }
