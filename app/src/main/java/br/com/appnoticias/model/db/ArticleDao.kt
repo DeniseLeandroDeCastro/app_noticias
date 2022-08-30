3a481aab4930fe34b5e76cd941b7f37df4bbd90c
DeniseLeandroDeCastro <dmaria.natal@gmail.com>
2022-08-30 20:20:35 -0300

    Implementando a classe ArticleDao

--- app/src/main/java/br/com/appnoticias/model/db/ArticleDao.kt
+++ app/src/main/java/br/com/appnoticias/model/db/ArticleDao.kt
@@ -1,4 +1,17 @@
 package br.com.appnoticias.model.db
 
-class ArticleDao {
+import androidx.room.*
+import br.com.appnoticias.model.Article
+
+@Dao
+interface ArticleDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun updateInsert(article: Article) : Long
+
+    @Query("SELECT * FROM articles")
+    fun getAll(): List<Article>
+
+    @Delete
+    suspend fun delete(article: Article)
 }
