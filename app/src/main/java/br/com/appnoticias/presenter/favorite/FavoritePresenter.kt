507b28fb57f35913bc5bd3f05a3b84f0c44566b6
DeniseLeandroDeCastro <dmaria.natal@gmail.com>
2022-08-31 20:38:41 -0300

    Listando as notícias favoritas

--- app/src/main/java/br/com/appnoticias/presenter/favorite/FavoritePresenter.kt
+++ app/src/main/java/br/com/appnoticias/presenter/favorite/FavoritePresenter.kt
@@ -8,6 +8,10 @@ class FavoritePresenter(
     val view: ViewHome.Favorite,
     private val dataSource: NewsDataSource): FavoriteHome.Presenter {
 
+    fun getAll() {
+        this.dataSource.getAllArticle(this)
+    }
+
     fun saveArticle(article: Article) {
         this.dataSource.saveArticle(article)
     }
--- app/src/main/java/br/com/appnoticias/ui/FavoriteActivity.kt
+++ app/src/main/java/br/com/appnoticias/ui/FavoriteActivity.kt
@@ -1,11 +1,56 @@
 package br.com.appnoticias.ui
 
+import android.content.Intent
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import br.com.appnoticias.R
+import br.com.appnoticias.adapter.MainAdapter
+import br.com.appnoticias.model.Article
+import br.com.appnoticias.model.data.NewsDataSource
+import br.com.appnoticias.presenter.ViewHome
+import br.com.appnoticias.presenter.favorite.FavoritePresenter
+import kotlinx.android.synthetic.main.activity_favorite.*
+import kotlinx.android.synthetic.main.activity_main.*
 
-class FavoriteActivity : AbstractActivity() {
+class FavoriteActivity : AbstractActivity(), ViewHome.Favorite {
+
+    private val mainAdapter by lazy {
+        MainAdapter()
+    }
+
+    private lateinit var presenter: FavoritePresenter
+
     override fun getLayout(): Int = R.layout.activity_favorite
 
     override fun onInject() {
+        val dataSource = NewsDataSource(this)
+        presenter = FavoritePresenter(this, dataSource)
+        presenter.getAll()
+        configRecycle()
+        clickAdapter()
+    }
 
+    private fun configRecycle() {
+        with(rvFavorite) {
+            adapter = mainAdapter
+            layoutManager = LinearLayoutManager(this@FavoriteActivity)
+            addItemDecoration(
+                DividerItemDecoration(
+                this@FavoriteActivity, DividerItemDecoration.VERTICAL
+            )
+            )
+        }
+    }
+
+    private fun clickAdapter() {
+        mainAdapter.setOnClickListener { article ->
+            val intent = Intent(this, ArticleActivity::class.java)
+            intent.putExtra("article", article)
+            startActivity(intent)
+        }
+    }
+
+    override fun showArticles(articles: List<Article>) {
+        mainAdapter.differ.submitList(articles.toList())
     }
 }
